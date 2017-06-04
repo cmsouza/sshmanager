6a838577ad329555b0887c524e72a652b3fe39cb
Ciro Souza <ciro.mies@gmail.com>
2017-06-04 16:22:29 -0300

    Adicionando model

--- sshmanager/models.py
+++ sshmanager/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Host(models.Model):
+    id = models.AutoField(primary_key = True)
+    owner = models.ForeignKey('auth.User')
+    host = models.CharField(max_length = 100)
+    hostname = models.CharField(max_length = 100)
+    user = models.CharField(max_length = 100)
+    port = models.IntegerField()
+    active = models.BooleanField(default = True)
+    created_at = models.DateTimeField(default = timezone.now)
+
+    def __str__(self):
+        return "%s@%s:%d" % (self.user, self.hostname, self.port)
