3fdafb355b3712a77ab21c4e46619b458b2bc708
Ciro Souza <ciro.mies@gmail.com>
2017-06-04 16:49:59 -0300

    Allowed Hosts

--- manager/settings.py
+++ manager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*en(u^5--t7v5ex*e=fr-*=58agm%++ix@4^*&5%44bn24jpy^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
