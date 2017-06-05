ae9628a288678935e84ea6cc3f78c165069c8ef2
Ciro Souza <ciro.souza@grupofolha.com.br>
2017-06-05 17:07:02 -0300

    Add api

--- manager/urls.py
+++ manager/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include('sshmanager.urls'))
+    url(r'', include('sshmanager.urls')),
+    url(r'^api/', include('api.urls')),
 ]
