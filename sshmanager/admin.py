ab0933a88cbe6abbf9ab492c41c3a6cdfdc4e2ec
Ciro Souza <ciro.mies@gmail.com>
2017-06-06 02:16:48 -0300

    user and timestamp readonly and automatic

--- sshmanager/admin.py
+++ sshmanager/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 from .models import Host
 
 # Register your models here.
-admin.site.register(Host)
+
+class HostAdmin(admin.ModelAdmin):
+	readonly_fields = ('owner','created_at',)
+	list_filter = ('owner',)
+	search_fields = ['host', 'owner__username']
+
+	def save_model(self, request, obj, form, change):
+		obj.owner = request.user
+		obj.save()
+
+admin.site.register(Host, HostAdmin)
