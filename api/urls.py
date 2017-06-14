c9ef7d3cfbeb1f26c6d71daaa0dfa20245bf1d28
Ciro Souza <ciro.mies@gmail.com>
2017-06-14 01:54:51 -0300

    pep8

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from . import views
 
+
 urlpatterns = [
     url(r'^configFile$', views.config_file, name='config_file'),
 ]
--- api/views.py
+++ api/views.py
@@ -3,19 +3,19 @@ from django.http.response import HttpResponseServerError, Http404
 from django.shortcuts import render
 from sshmanager.models import Token, Host
 
-# Create your views here.
+
 def config_file(request):
-	if 'HTTP_X_API_TOKEN' not in request.META:
-		return HttpResponseServerError('Token Header not present')
+    if 'HTTP_X_API_TOKEN' not in request.META:
+        return HttpResponseServerError('Token Header not present')
 
-	try:
-		token = Token.objects.get(token=request.META['HTTP_X_API_TOKEN'])
-	except ObjectDoesNotExist:
-		raise Http404('Invalid Token')
+    try:
+        token = Token.objects.get(token=request.META['HTTP_X_API_TOKEN'])
+    except ObjectDoesNotExist:
+        raise Http404('Invalid Token')
 
-	hosts = Host.objects.filter(owner=token.owner, active=True)
+    hosts = Host.objects.filter(owner=token.owner, active=True)
 
-	response = render(request, 'config_file.html', {'hosts': hosts})
-	response.content_type = 'text/plain'
+    response = render(request, 'config_file.html', {'hosts': hosts})
+    response.content_type = 'text/plain'
 
-	return response
+    return response
